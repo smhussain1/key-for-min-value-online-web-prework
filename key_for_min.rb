9939ba4d11b3c7b2b94641305553b194f4005d2a
michael hussain <smhussain_1@yahoo.com>
2019-01-03 23:41:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,16 @@ def key_for_min_value(h)
   lowest_key = nil 
   lowest_value = nil 
   h.each do |key, value|
-    
+    if value == lowest_value || value < lowest_value
+      lowest_value = value
+      lowest_key = key 
+    end
     puts h
-    smallest_value_key = key 
 end
 end 
 
+#test if the lowest_value is nil, or if the value is less than the lowest_value
+    	# if yes, reset the lowest_key and lowest_value variables to the key and value
+    
+end #after the loop return the lowest_key
+end
